b80960fed05688efead988c23ba7933bde25160d
Kendall White <63067332+kendallw763@users.noreply.github.com>
2026-08-04 13:49:04 -0400

    Access Update
    Refactored main function to include user credentials and biometric scan. Added DeviceCount functionality.

--- network.py
+++ network.py
@@ -1,25 +1,25 @@
-from time import time
-from server.server import firewall, router, server, switch, WAP
-from access.access import userAccess
-import sys
-
-def main():
-    userAccess_obj = userAccess()
+from server.server import firewall, router, server, switch, WAP, DeviceCount
+from access.access import Credentials, BiometricScan
+#=======================#=======================#=======================#=======================
+def main(): 
+    credentials_obj = Credentials()
+    biometricScan_obj = BiometricScan()
     firewall_obj = firewall()
     router_obj = router()
     server_obj = server()
     switch_obj = switch()
     wap_obj = WAP()
-
-    if userAccess_obj.biometric() is False:
-        print("You are not authorized...")
-        sys.exit()
-        
+    deviceCount_obj = DeviceCount()
+#=======================#=======================#=======================#=======================
+    credentials_obj.userPassCredentials()
+    biometricScan_obj.biometric()
     firewall_obj.firewallConnection()
     router_obj.routerConnection()
     server_obj.ServerConection()
     switch_obj.switchConection()
     wap_obj.wapConnection()
-  
+    deviceCount_obj.deviceCount()
+#=======================#=======================#=======================#=======================
 if __name__ == '__main__':
     main()
+    
